0a46252ae186e1ccaa83b0fdbb39c632e9c64073
OOLT.VN.20202.20190051.TranTrongHiep <trantronghiep2001@gmail.com>
2022-01-06 17:21:58 +0700

    ql nha van hoa

---
+++ QuanLyNhaVanHoa/src/services/MysqlConnection.java
@@ -0,0 +1,30 @@
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Hai
+ */
+public class MysqlConnection {
+    public static Connection getMysqlConnection() throws SQLException, ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName = "quan_ly_nhan_khau";
+        String userName = "postgres";
+        String password = "passwd";
+        return getMysqlConnection(hostName, dbName, userName, password);
+    }
+    
+    public static Connection getMysqlConnection(String hostName, String dbName, String userName, String password) 
+        throws SQLException, ClassNotFoundException{
+        //Class.forName("com.mysql.jdbc.Driver");
+        String connectionUrl = "jdbc:postgresql://localhost:5432/quan_ly_nhan_khau";;
+        System.out.println(connectionUrl);
+        Connection conn = DriverManager.getConnection(connectionUrl, userName, password);
+        if(conn != null) System.out.println("Connected!");
+        else System.out.println("Failed");
+        return conn;
+    }
+}
